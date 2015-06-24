23a6be3eb4c8c4efef94cb8cc750c9aa8736f580
ndrozd <iam@zemel.info>
2015-06-24 13:58:17 +0200

    1 page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function($) {
-
+	$(".can-animate").on('change', '.nd-switch input', function(event) {
+		event.preventDefault();
+		if ($(this).is(":checked")) {
+			$(this).closest(".can-animate").removeClass('active unactive').addClass('anim-active').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(event) {
+				$(this).addClass('active');
+				$(this).removeClass('anim-active');
+			});;
+		}
+		else {
+			$(this).closest(".can-animate").removeClass('active unactive').addClass('anim-unactive').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(event) {
+				$(this).addClass('unactive');
+				$(this).removeClass('anim-unactive');
+			});;
+		}
+	});
+	$("body").on('click', '.js-toggleactive', function(event) {
+		event.preventDefault();
+		$(this).toggleClass('active');
+	});
 });
